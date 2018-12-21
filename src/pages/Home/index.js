ac31afd28255be51e64f3286284f3ddc8b900754
Renan Machado <renan.mav@hotmail.com>
2018-12-21 12:14:38 -0300

    Home creation and ESLint final config

---
+++ src/pages/Home/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+// import { Container } from './styles';
+
+export default class Home extends Component {
+  
+  render() {
+    return <div />;
+  }
+}
