6cd6566916f4264e4aa004158be0ad8fdfd6d4d1
Renan Machado <renan.mav@hotmail.com>
2019-01-12 00:30:43 -0300

    Admin panel sketch

--- src/pages/Panel/index.js
+++ src/pages/Panel/index.js
@@ -1,7 +1,34 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import {
+  FaPlus, FaSignOutAlt, FaPen, FaTrash,
+} from 'react-icons/fa';
 
-const Panel = () => <div><h1>Panel</h1></div>;
+import {
+  Container, SideBar, Items, Item,
+} from './styles';
+
+const Panel = () => (
+  <Container>
+    <SideBar>
+      <Items>
+        <Item href="#" texto="Criar um post">
+          <FaPlus />
+        </Item>
+        <Item href="#" texto="Editar um post">
+          <FaPen />
+        </Item>
+        <Item href="#" texto="Deletar um post">
+          <FaTrash />
+        </Item>
+      </Items>
+      <Items>
+        <Item href="#" texto="Sair">
+          <FaSignOutAlt />
+        </Item>
+      </Items>
+    </SideBar>
+  </Container>
+);
 
 export default Panel;
---
+++ src/pages/Panel/styles.js
@@ -0,0 +1,77 @@
+import styled from 'styled-components';
+
+import { colors, metrics } from '~/styles';
+
+export const Container = styled.div`
+  display: flex;
+  flex: 1;
+  width: 100vw;
+  height: 100vh;
+
+  background-color: ${colors.darker};
+`;
+
+export const SideBar = styled.div`
+  height: 100%;
+  width: 43px;
+
+  border-right: 1px solid ${colors.dark};
+  margin-right: ${metrics.baseMargin}px;
+  padding: ${metrics.baseMargin}px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+export const Items = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Item = styled.a`
+  text-decoration: none;
+  color: ${colors.white};
+
+  font-weight: bold;
+  font-size: 1.1em;
+
+  margin-bottom: ${metrics.baseMargin * 1.2}px;
+
+  position: relative;
+
+  z-index: 2;
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+
+  &:before {
+      content: '${props => props.texto || ''}';
+
+      white-space:nowrap;
+
+      top: -7px;
+      left: -10px;
+
+      background: ${colors.darkTransparent};
+      border-radius: ${metrics.baseRadius}px;
+
+      padding: 5px 0;
+      padding-left: 40px;
+      padding-right: 15px;
+
+      position: absolute;
+      z-index: -1;
+
+      opacity: 0;
+      transition: opacity 0.3s ease-in-out;
+    }
+
+    &:hover:before{
+      opacity: 1;
+      transition-delay: 1s;
+      -webkit-transition-delay: 1s; /* Safari */
+    }
+`;
