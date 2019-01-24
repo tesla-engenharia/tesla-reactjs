34373903181b87ac852fbca8f0e57e4d168eabe7
Renan Machado <renan.mav@hotmail.com>
2019-01-23 22:43:28 -0300

    Not found page stylingish

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 
-// import { Container } from './styles';
+import { Container } from "./styles";
+
+import error from "~/assets/error.png";
 
 const NotFound = () => (
-  <div>
-    <h1>Essa página não existe!</h1>
-  </div>
+  <Container>
+    <img src={error} alt="Página 404" />
+    <p>Parece que você está tentando acessar uma página que não existe.</p>
+  </Container>
 );
 
 export default NotFound;
