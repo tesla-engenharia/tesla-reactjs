0e281741396bf4122a6e2b0e2845c6964755051e
Mateus Rodrigues <mateussr2511@gmail.com>
2019-11-04 11:04:10 -0300

    Saiba mais button added to services

--- src/pages/Servicos/list.js
+++ src/pages/Servicos/list.js
@@ -12,22 +12,18 @@ export default function ListServicos({ lista }) {
       </TitleWrapper>
       <ContentWrapper>
         {lista.map(service => (
-          <Link
-            to={"/servicos/" + service.id}
-            style={{ textDecoration: "none" }}
-          >
-            <Service itemtype="https://schema.org/Service">
-              <p itemprop="name">{service.title}</p>
-              <img
-                itemprop="image"
-                src={
-                  process.env.REACT_APP_API_URL + "/files/" + service.icon_id
-                }
-                alt={service.title}
-              />
-              <span itemprop="description">{service.description}</span>
-            </Service>
-          </Link>
+          <Service itemtype="https://schema.org/Service">
+            <p itemprop="name">{service.title}</p>
+            <img
+              itemprop="image"
+              src={process.env.REACT_APP_API_URL + "/files/" + service.icon_id}
+              alt={service.title}
+            />
+            <span itemprop="description">{service.description}</span>
+            <Link to={"/servicos/" + service.id}>
+              <h3>Saiba mais</h3>
+            </Link>
+          </Service>
         ))}
       </ContentWrapper>
     </Container>
